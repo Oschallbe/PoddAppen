696d47a9c789dbc0be9bdb3ab607c2874f26866a
LinoDeLuca <linodeluca2004@gmail.com>
2025-11-17 12:57:03 +0100

    fix med rss

---
+++ PoddApp.DAL/RSSPodd.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ServiceModel.Syndication;
+using System.Xml;
+using PoddApp.Models;
+
+namespace PoddApp.DAL
+{
+    public class RSSPodd
+    {
+        private HttpClient aHttpClient;
+
+
+        public RSSPodd(HttpClient httpClient)
+        {
+            this.aHttpClient = httpClient;
+        }
+
+        public async Task<List<Episode>> HentRSSFeed(string rssUrl)
+        {
+            Stream dataStream = await aHttpClient.GetStreamAsync(rssUrl);
+            XmlReader myReader = XmlReader.Create(dataStream);
+            SyndicationFeed dataFlow = SyndicationFeed.Load(myReader);
+        }
+
+    }
+}
