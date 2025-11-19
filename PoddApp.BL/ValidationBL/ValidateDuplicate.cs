088608e937976cf65e287448149a2d20753b97b8
joeljansson505 <jooeell5@gmail.com>
2025-11-19 11:09:58 +0100

    va

--- PoddApp.BL/ValidationBL/ValidateDuplicate.cs
+++ PoddApp.BL/ValidationBL/ValidateDuplicate.cs
@@ -3,10 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PoddApp.DAL;
+using PoddApp.Models;
 
 namespace PoddApp.BL.ValidationBL
 {
-    internal class ValidateDuplicate
+    public class ValidateDuplicate
     {
+        private readonly PodcastRepository podcastRepository;
+
+        public ValidateDuplicate(PodcastRepository podcastRepository)
+        {
+            this.podcastRepository = podcastRepository;
+        }
+
+        public async Task<ValidateUrl> ValidateDuplicateAsync(String rssUrl)
+        {
+            var result = new ValidateUrl();
+
+            var existing = await this.podcastRepository.GetByRssUrlAsync(rssUrl);
+
+            if (existing != null)
+            {
+                result.AddError("Denna podcast finns redan sparad");
+            }
+
+            return result;
+        }
     }
 }
