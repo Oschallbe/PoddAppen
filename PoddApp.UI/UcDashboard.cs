44a762928e9eb44780d3bd5f20066efb295622c2
Oschallbe <hallbergoscarr@gmail.com>
2025-11-19 10:59:55 +0100

    lista alla poddar

--- PoddApp.UI/UcDashboard.cs
+++ PoddApp.UI/UcDashboard.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using PoddApp.DAL;
+using PoddApp.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +14,35 @@ namespace PoddApp.UI
 {
     public partial class UcDashboard : UserControl
     {
-        public UcDashboard()
+        private readonly IPodcastRepo _repo;
+        private List<Podcast> _podcasts = new();
+
+        public UcDashboard(IPodcastRepo repo)
         {
             InitializeComponent();
+            _repo = repo;
+
+            _ = LoadPodcastsAsync();
+        }
+
+        private async Task LoadPodcastsAsync()
+        {
+            try
+            {   
+                _podcasts = await _repo.GetAllAsync() ?? new List<Podcast>();
+
+                lbMyPod.Items.Clear();
+
+                foreach (var p in _podcasts)
+                {
+                    string title = string.IsNullOrEmpty(p.Name) ? p.RssUrl : p.Name;
+                    lbMyPod.Items.Add(title);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Kunde inte ladda poddar: " + ex.Message);
+            }
         }
 
         private void btnDeletePod_Click(object sender, EventArgs e)
