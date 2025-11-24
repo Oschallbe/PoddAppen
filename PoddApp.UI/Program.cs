00a74721e6e78bb94353521c289f567806ddecea
LinoDeLuca <linodeluca2004@gmail.com>
2025-11-24 13:18:10 +0100

    Update Program.cs

--- PoddApp.UI/Program.cs
+++ PoddApp.UI/Program.cs
@@ -38,7 +38,7 @@ namespace PoddApp.UI
             IValidation validation = new Validation(repo);
 
 
-            // Starta appen och skicka in service 
+            // Starta appen 
             ApplicationConfiguration.Initialize();
             Application.Run(new MainForm(service, validation, repo));
         }
